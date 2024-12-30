daa076f119c7983e78ecdad567cbe15e88dfb22d
ishadiIFSolutions <76896498+HMDITharushika@users.noreply.github.com>
2024-12-30 22:50:45 +0530

    UI Enhancements

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,25 @@
-import Navbar from "../../components/Navbar/Navbar"
+import NoteCard from "../../components/Cards/NoteCard";
+import Navbar from "../../components/Navbar/Navbar";
 
 const Home = () => {
   return (
     <>
-    <Navbar />
-    </>
-  )
-}
+      <Navbar />
 
-export default Home
+      <div className="container mx-auto">
+        <NoteCard
+          title="Meeting on 7th January"
+          date="30th Dec 2024"
+          content="Meeting on 7th January"
+          tags="#Meeting"
+          isPinned={true}
+          onEdit={() => {}}
+          onDelete={() => {}}
+          onPinNote={() => {}}
+        />
+      </div>
+    </>
+  );
+};
+
+export default Home;
